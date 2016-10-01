9e14d5ca4d8de668ef618c5abb330d8eb714b486
Olga Zelenskaya <ol.v.zelenskaya@gmail.com>
2016-10-01 14:00:00 +0300

    Adding 2 files from folder New

---
+++ src/main/java/com/olgazelenskaya/app/App.java
@@ -0,0 +1,13 @@
+package com.olgazelenskaya.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
