2a7586fed58a89dee42b38d934091a22620aa129
Olga Zelenskaya <ol.v.zelenskaya@gmail.com>
2016-10-08 00:09:47 +0300

    Apptwo byte code file added

---
+++ src/main/java/com/olgazelenskaya/app/Appone.java
@@ -0,0 +1,7 @@
+package com.olgazelenskaya.app;
+
+/**
+ * Created by PC on 06.10.2016.
+ */
+public class Appone {
+}
---
+++ src/test/java/com/olgazelenskaya/app/Apptwo.java
@@ -0,0 +1,16 @@
+package com.olgazelenskaya.app;
+
+/**
+ * Created by PC on 06.10.2016.
+ */
+import java.util.Scanner;
+
+public class Apptwo {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println(scanner.nextLine());
+    }
+}
