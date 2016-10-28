fa0434a4f00a3d6ff6922ed7010abe8764cc690c
Olga Zelenskaya <ol.v.zelenskaya@gmail.com>
2016-10-29 00:53:18 +0300

    Homework to lesson 7. Changes made

--- src/main/java/com/olgazelenskaya/runners/homework/lesson7/ArrayClass.java
+++ src/main/java/com/olgazelenskaya/runners/homework/lesson7/ArrayClass.java
@@ -4,32 +4,40 @@ package com.olgazelenskaya.runners.homework.lesson7;
  * Created by PC on 25.10.2016.
  */
 public class ArrayClass {
-//    public static void main(String[] args) {
-//        int[] a = new int[10];
-//        for (int i = 0; i < a.length; i ++) {
-//            a[i] = (i+1)*2;
-//               System.out.println(a[i]+ " ");
-//            }
-//        }
-//    }
-//    public static void main(String[] args) {
-//        int[] a = new int[50];
-//        for (int i = 0; i < a.length; i++) {
-//            a[i] = i*2+1;
-//
-//            System.out.print(a[i] + " ");
-//        }
-//        System.out.println(" ");
-//        for(int i = a.length-1; i >= 0; i--){
-//            System.out.print(a[i] + " ");
-//
-////        for (int i = 0; i < a.length; i++){
-////            System.out.print(a[a.length-1-i] + " ");
-//        }
-//
-//    }
 
     public static void main(String[] args) {
+    //    public void taskOne() {
+
+        int[] a = new int[10];
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (i + 1) * 2;
+            System.out.println(a[i] + " ");
+        }
+    }
+
+    public void taskTwo() {
+    //    public static void main(String[] args) {
+
+        int[] a = new int[50];
+
+            for (int i = 0; i < a.length; i++) {
+            a[i] = i * 2 + 1;
+
+            System.out.print(a[i] + " ");
+        }
+        System.out.println(" ");
+
+        for (int i = a.length - 1; i >= 0; i--) {
+            System.out.print(a[i] + " ");
+
+    //      for(int i = 0; i < a.length; i++){
+    //      System.out.print(a[a.length-1-i] + " ");
+        }
+   }
+
+    public void taskThree() {
+    //   public static void main(String[] args) {
+
         int[] a = new int[15];
         int evenNumbers = 0;
 
@@ -39,32 +47,52 @@ public class ArrayClass {
 
             if ((a[i] % 2 == 0) && (a[i] != 0)) {
                 evenNumbers++;
+                }
             }
+            System.out.println(" ");
+            System.out.println(" ");
+            System.out.println("Ответ: ");
+            System.out.println("Четных элементов: " + evenNumbers);
+            }
+
+    public void taskFour() {
+   // public static void main(String[] args) {
+
+        int[] a = new int[15];
+
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (int) (Math.random() * 998 + 1);
+            System.out.print(a[i] + " ");
+            System.out.println(" ");
+
+            int min = a[0];
+            int max = min;
+
+            for (i = 1; i < a.length; ++i) {
+                if (a[i] > max) max = a[i];
+                else if (a[i] < min) min = a[i];
+            }
+            System.out.println(" ");
+            System.out.println("Ответ:");
+            System.out.println("Минимальное число - это " + min + ";");
+            System.out.println("Максимальное число - это " + max + ".");
         }
-        System.out.println(" ");
-        System.out.println(evenNumbers);
+    }
+
+    public void taskFive(){
+   // public static void main(String[] args) {
+
+        int[][] a = new int[8][5];
+
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                a[i][j] = (int) (Math.random() * 90) + 10;
+                System.out.print(a[i][j] + " ");
+            }
+            System.out.println(" ");
+        }
+            System.out.println(" ");
+            System.out.println("Ответ:");
+            System.out.println("Восемь строк и пять столбцов");
     }
 }
-
-//    public static void main(String[] args) {
-//        int[] a = new int[15];
-//        int max = a[0];
-//        int min = a[0];
-//
-//        for (int i = 0; i < a.length; i++) {
-//
-//            a[i] = (int) (Math.random() * 998 + 1);
-//            System.out.print(a[i] + " ");}
-//
-//            for (int i = 0; i != a.length; i++) {
-//                if (a[i] > max) {
-//                    max = a[i];
-//                }
-//                if (a[i] < min) {
-//                    min = a[i];
-//                }
-//                System.out.println(min + " " + max);
-//
-//        }
-//    }
-//}
